7d8ca6ddbdd9179667d4f0bd2947135f79fbd71c
qdnerak <neraknerakneraknerak@gmail.com>
2020-08-30 18:03:27 -0400

    Practica2

---
+++ app/src/main/java/com/qdnerak/contacus/contacUs.java
@@ -0,0 +1,14 @@
+package com.qdnerak.contacus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class contacUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contac_us);
+    }
+}
